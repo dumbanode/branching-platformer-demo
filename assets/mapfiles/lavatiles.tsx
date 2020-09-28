2222608811be81035fdb982af5a0c08daa4e48df
dominode <smac1994@gmail.com>
2020-09-27 20:10:03 -0700

    Initial Commit

---
+++ assets/mapfiles/lavatiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="lavatiles" tilewidth="8" tileheight="8" tilecount="24" columns="6">
+ <image source="lavasprites.png" width="48" height="32"/>
+</tileset>
